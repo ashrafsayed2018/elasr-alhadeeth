b6d0ff833c6b0a8c0c8748d4140ad862fd51bcc7
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2023-12-18 23:05:55 +0200

    adding stork width to 2 in menu button in mobile

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -26,7 +26,7 @@ function Navbar() {
             </span>
 
             <label htmlFor="check" className="open-menu animate-bounce">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="blue" className="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="blue" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
 
