99c3404bcc71ace0b2764171a712a780ed81b9a4
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2023-12-18 23:03:48 +0200

    adding animation to menu icon in mobile to bounce

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -25,8 +25,8 @@ function Navbar() {
               </label>
             </span>
 
-            <label htmlFor="check" className="open-menu">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <label htmlFor="check" className="open-menu animate-bounce">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="blue" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
 
