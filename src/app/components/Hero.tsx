edbad57861ea6868c599a4ceb4a56f23464ea6e1
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2024-01-12 22:01:19 +0200

    adding margin top to home page only using useParams hook

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,11 +1,15 @@
+"use client"
 import Link from "next/link"
 import { ContactInfo } from "../constants"
-
+import { useParams } from "next/navigation"
 function Hero() {
+
+  const params = useParams()
+  
   return (
-    <div className="text-center flex flex-col gap-8 h-auto">
+    <div className={`text-center flex flex-col gap-8 h-auto ${params.id == undefined ? "mt-80" : ""}`}>
         <h1 className="text-blue-900 font-bold text-xl">العصر الحديث للمقاولات</h1>
-        <p className="text-[#800000] font-bold text-xl">مقاول ترميم بناء ملاحق صباغ </p>
+        <p className="text-[#933a3a] font-bold text-xl">مقاول ترميم بناء ملاحق صباغ </p>
         <p className="text-[#993366] font-bold text-xl">ترميم مباني بناء ملاحق اصباغ اصباغ خارجيه\ اصباغ داخليه \ ازالة اصباغ قديمة \ اعمال بروفايل \ كسر ركام</p>
         <p className="text-[#993300] font-bold text-xl">تركيب عوازل اسطح \ ديكورات \ بناء مجالس خارجيه \ بناء ملاحق \ ترميمي شامل \ تركيب اسفلت وايبوكسي \ مظلات - سواتر - برجولات -عشب صناعي</p>
         <p className="text-[#ff0000] font-bold text-xl">لدينا عماله مهنية مدربه على مستوى من الخبره والاحترافيه في انجاز العمل المطلوب باكمل وجه</p>
