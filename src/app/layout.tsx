dd7dcd42080f19058fdb5d05a20341338d9bdb0e
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2024-05-25 00:32:10 +0300

    modifying the site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'مقاول ترميم بناء ملاحق صباغ',
-  description: 'ترميم مباني بناء ملاحق اصباغ اصباغ خارجيه\ اصباغ داخليه \ ازالة اصباغ قديمة \ اعمال بروفايل \ كسر ركام',
-}
+  title: "مقاول ترميم بناء ملاحق صباغ",
+  description:
+    "ترميم مباني بناء ملاحق اصباغ اصباغ خارجيه اصباغ داخليه  ازالة اصباغ قديمة  اعمال بروفايل  كسر ركام",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="ar" dir='ltr' className='scroll-smooth overflow-x-hidden'>
+    <html lang="ar" dir="rtl" className="scroll-smooth overflow-x-hidden">
       <body className="overflow-x-hidden">{children}</body>
     </html>
-  )
+  );
 }
